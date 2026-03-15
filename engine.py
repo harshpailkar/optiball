f27507fc4983338dfe6b31409df98b4afc75a67e
Harsh Pailkar <85762743+harshpailkar@users.noreply.github.com>
2026-03-15 23:40:46 +0530

    implement Value class for automatic differentiation

---
+++ engine.py
@@ -0,0 +1,84 @@
+class Value:
+
+  def __init__(self, data, _children=(), _op=''):
+      self.data = data
+      self.grad = 0
+      self._backward = lambda: None
+      self._prev = set(_children)
+      self._op = _op
+
+  def __add__(self, other):
+      other = other if isinstance(other, Value) else Value(other)
+      out = Value(self.data + other.data, (self, other), '+')
+
+      def _backward():
+          self.grad += out.grad
+          other.grad += out.grad
+      out._backward = _backward
+      return out
+
+  def __mul__(self, other):
+      other = other if isinstance(other, Value) else Value(other)
+      out = Value(self.data * other.data, (self, other), '*')
+
+      def _backward():
+          self.grad += other.data * out.grad
+          other.grad += self.data * out.grad
+      out._backward = _backward
+      return out
+
+  def __pow__(self, other):
+      out = Value(self.data ** other, (self,), f'**{other}')
+
+      def _backward():
+          self.grad += (other * self.data**(other-1)) * out.grad
+      out._backward = _backward
+      return out
+
+  def exp(self):
+      out = Value(math.exp(self.data), (self,), 'exp')
+
+      def _backward():
+          self.grad += out.data * out.grad
+      out._backward = _backward
+      return out
+
+  def cos(self, freq=1.0):
+      x = self.data * freq
+      out = Value(math.cos(x), (self,), 'cos')
+
+      def _backward():
+          self.grad += (-math.sin(x)*freq) * out.grad
+      out._backward = _backward
+      return out
+
+  def sqrt(self):
+      out = Value(self.data**0.5, (self,), 'sqrt')
+
+      def _backward():
+          self.grad += (0.5 * self.data**-0.5) * out.grad
+      out._backward = _backward
+      return out
+
+  def backward(self):
+
+      topo = []
+      visited = set()
+
+      def build(v):
+          if v not in visited:
+              visited.add(v)
+              for c in v._prev:
+                  build(c)
+              topo.append(v)
+
+      build(self)
+
+      self.grad = 1
+      for v in reversed(topo):
+          v._backward()
+
+  def __neg__(self): return self * -1
+  def __sub__(self, other): return self + (-other)
+  def __radd__(self, other): return self + other
+  def __rmul__(self, other): return self * other
